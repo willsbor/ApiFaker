acd5bff060661a52b468c421663c53d6f7cb1fa2
willsborKang <willsbor@gmail.com>
2014-01-08 02:25:01 +0800

    config for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiFaker::Application.config.secret_token = 'afa589f2c5114064db44ef311f3c4a11b08a9d97ab2b1841a03363c6f52291c82f14afc9042d0aaec2b2397d38e1bf52a0f3a2df134fdb57a8f268e5f5aedb03'
